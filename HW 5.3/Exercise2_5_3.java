c87d1d7af8c260057d649bc7beb3b92497e981d9
SimonaIvanovaPetrova <simonaivanovapetrova@gmail.com>
2026-06-29 18:48:26 +0300

    Add files via upload

---
+++ HW 5.3/Exercise2_5_3.java
@@ -0,0 +1,13 @@
+package HW_4_8;
+
+public class Exercise2_5_3 {
+    public static void main(String[] args) {
+        avgNumbers(5,10);
+    }
+    //Да се състави метод, който приема като аргументи две числа и отпечатва на екрана техния сбор.
+    public static void   avgNumbers(int i,int t){
+        double result=0.0;
+        System.out.println(result=(i+t)/2.0);
+
+    }
+}
---
+++ HW 5.3/Exercise6_5_3.java
@@ -0,0 +1,20 @@
+package HW_4_8;
+
+import java.util.Arrays;
+
+public class Exercise6_5_3 {
+    public static void main(String[] args) {
+
+
+        //Задача 6
+        //Дадени са два масива със стойности {1, 2, 3} и  {1, 2, 3}
+        // да се състави програма, която проверява дали масивите са еднакви
+        int[] firstArray = {1, 2, 3};
+        int[] secondArray = {1, 2, 3};
+        if(Arrays.equals(firstArray,secondArray)){
+            System.out.println("Equals");
+        }else{
+            System.out.println("Not equal.");
+        }
+    }
+}
